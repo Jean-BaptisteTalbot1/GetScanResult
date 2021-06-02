c79a6f1639ba59fa03e568f724a99f9d8ecbfe35
Jean-BaptisteTalbot1 <77397124+Jean-BaptisteTalbot1@users.noreply.github.com>
2021-06-02 09:37:48 -0400

    Add files via upload

---
+++ PaperWorks_GetScanResultsXML/PaperWorks_GetScanResultsXML/Barcode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperWorks_GetScanResultsXML
+{
+    class Barcode : IEquatable<Barcode>
+    {
+        /* Getter */
+        public string Code { get; }
+        public BarcodeTypes Type { get; }
+
+        public Barcode(string code, BarcodeTypes type)
+        {
+            Code = code;
+            Type = type;
+        }
+
+        /* Equality members */
+        public bool Equals(Barcode other)
+        {
+            if (ReferenceEquals(null, other))
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            return Code == other.Code && Type == other.Type;
+        }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj.GetType() != GetType())
+                return false;
+            return Equals((Barcode)obj);
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Code != null ? Code.GetHashCode() : 0) * 397) ^ (int)Type;
+            }
+        }
+        public static bool operator ==(Barcode left, Barcode right)
+        {
+            return Equals(left, right);
+        }
+        public static bool operator !=(Barcode left, Barcode right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ PaperWorks_GetScanResultsXML/PaperWorks_GetScanResultsXML/BarcodeTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperWorks_GetScanResultsXML
+{
+    public enum BarcodeTypes
+    {
+        /*  Enumeration to list the barcode types available on the application
+    UPC : Universal Product Code
+    ITF : Interleaved 2 of 5 */
+        Upc,
+        Itf,
+        DataMatrix,
+        Ean,
+        Unknown
+    }
+}
